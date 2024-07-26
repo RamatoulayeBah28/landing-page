d71e1df8662493a2e0f894a5bd6ce5b384b74d6e
Ramatoulaye Bah <141573185+RamatoulayeBah28@users.noreply.github.com>
2024-07-26 13:36:02 +0200

    added resume

---
+++ send_mail.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = 'Ramatoulaye.bah2805@gmail.com';
+    $subject = 'New Contact Form Submission';
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    $email_message = "<html><body>";
+    $email_message .= "<h2>Contact Form Submission</h2>";
+    $email_message .= "<p><strong>Name:</strong> {$name}</p>";
+    $email_message .= "<p><strong>Email:</strong> {$email}</p>";
+    $email_message .= "<p><strong>Message:</strong> {$message}</p>";
+    $email_message .= "</body></html>";
+
+    if (mail($to, $subject, $email_message, $headers)) {
+        echo "Message sent successfully";
+    } else {
+        echo "Error sending message";
+    }
+}
+
